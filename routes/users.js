bc24a3e2c443254950086afca5a2b353dcc16c8c
Ryan Kornyk <rkor23@gmail.com>
2015-11-18 14:37:01 -0500

    Revert "Final Commit for submission"
    This reverts commit 24f584af9cf94e1a1dad8a3cd4baa58c769411c4.

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+    var usernames = ['Me','You','Them']
+    //show the users.ejs in the browser
+    res.render('users', {title: 'Users',users: usernames, });
+});
+
+module.exports = router;
