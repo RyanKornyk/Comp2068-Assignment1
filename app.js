a7a4180ce7176b7a43c719b739de5f10ca734349
Ryan Kornyk <rkor23@gmail.com>
2015-11-18 16:54:36 -0500

    Fixes n Stuff
    Got code from github and added it to my project

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+// Registering npm modules
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+//additions for authentication
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var passport = require('passport');
+
+//DB Setup 
+var DB = require('./config/db.js');
+mongoose.connect(DB.url);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error');
+});
 
 var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
+
+require('./config/passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +38,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'someSecret',
+  saveUninitialized: true,
+  resave: true
+})
+);
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
+//app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+    res.render('index', {
+        title: 'Home'
+    });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+    res.render('about', {
+        title: 'About'
+    });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+    res.render('contact', {
+        title: 'Contact Me'
+    });
 });
 
 /* GET profile page. */
 router.get('/profile', function(req, res, next) {
-  res.render('profile', { title: 'Profile' });
+    res.render('profile', {
+        title: 'Profile'
+    });
 });
 
 /* GET service page. */
 router.get('/service', function(req, res, next) {
-  res.render('service', { title: 'Service' });
+    res.render('service', {
+        title: 'Service'
+    });
 });
+
+/* Render Login page. */
+router.get('/login', function(req, res, next) {
+    //if (!req.user) {
+        res.render('login', {
+            title: 'Login',
+            //messages: req.flash('loginMessage')
+            //displayName: req.user ? req.user.displayName : ''
+        });
+    //} else {
+    //    return res.redirect('/users');
+    //}
+});
+
+/* GET update page. */
+router.get('/register', function(req, res, next) {
+    res.render('register', {
+        title: 'Register'
+    });
+});
+
+/* GET business page. */
+router.get('/business', function(req, res, next) {
+    res.render('business', {
+        title: 'Business'
+    });
+});
+
 module.exports = router;
